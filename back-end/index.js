012461cd1d7e2c9c650a39a946faa911280a220e
Akshat Manohar <51690209+akshatmanohar21@users.noreply.github.com>
2025-01-02 15:59:20 +0530

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const axios = require("axios").default;
 
 const app = express();
-const PORT = process.env.PORT || 3001; // Use the provided port or default to 3001
+const PORT = process.env.PORT || 3001; 
 
 app.use(express.json());
 app.use(cors({ origin: true }));
@@ -19,19 +19,17 @@ app.post("/authenticate", async (req, res) => {
     );
     return res.status(r.status).json(r.data);
   } catch (e) {
-    console.error("Error:", e.message); // Log the error for debugging
+    console.error("Error:", e.message);
 
     if (e.response) {
-      // If the error has a response property
       return res.status(e.response.status).json(e.response.data);
     } else if (e.request) {
-      // If the error has a request property but no response (e.g., no response received)
+
       return res.status(500).json({
         message: "No response received from server",
         details: e.message,
       });
     } else {
-      // If the error is something else (e.g., a setup issue)
       return res
         .status(500)
         .json({ message: "Unexpected error", details: e.message });
